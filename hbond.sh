9ddde2486726eb6c85be89c8c332a4ab79898fba
Nayanika Das <94818947+ND7996@users.noreply.github.com>
2024-01-04 01:03:17 +0100

    Update hbond.sh

--- hbond.sh
+++ hbond.sh
@@ -1,17 +1,15 @@
 #! /bin/bash                      
 
-. /software/amber20/amber.sh
-rm trajin.in rmsf.dat rmsf.lig.dat
-echo "reference ./humancysgssg.inpcrd" >> trajin.in
-for i in `seq 0 20 5000`;do
-bzip2 -df humancysgssg.md$i.rst.bz2
-echo "trajin ./humancysgssg.md'$i'.rst" >> trajin.in
+. /software/amber20/amber.sh                                            
+
+echo "reference ./humancysgssg.inpcrd" >> trajin.in           #specify the coordinate file for simulation 
+for i in `seq 0 20 5000`;do                                   #specify the frames
+bzip2 -df humancysgssg.md$i.rst.bz2                           
+echo "trajin ./humancysgssg.md'$i'.rst" >> trajin.in          #Read in trajectory restart specified by filename
 done
-echo "autoimage
+echo "autoimage                                             #Automatically center and image a trajectory with periodic boundaries
 center origin :1-197
 hbond donormask :1-197 acceptormask :49 avgout hbond2.dat dist 3
 go" >> trajin.in
 
-cpptraj -p ./humancysgssg.prmtop < trajin.in
-
-
+cpptraj -p ./humancysgssg.prmtop < trajin.in                  #specify the prmtop file for simulation 
